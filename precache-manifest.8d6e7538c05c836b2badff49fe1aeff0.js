22300302a3aab712ec905de65806846b6ffb1f12
nir <reich.nir@gmail.com>
2020-08-04 15:22:13 +0300

    Updates

---
+++ precache-manifest.8d6e7538c05c836b2badff49fe1aeff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d15baf5ea7387cdc69b463f6e5fd67",
+    "url": "/currency-exchange-calculator/index.html"
+  },
+  {
+    "revision": "d3d00524fff0c355ce33",
+    "url": "/currency-exchange-calculator/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "63c34cd6fe6e7d556bd9",
+    "url": "/currency-exchange-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3d00524fff0c355ce33",
+    "url": "/currency-exchange-calculator/static/js/2.cfa7cfc9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/currency-exchange-calculator/static/js/2.cfa7cfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c34cd6fe6e7d556bd9",
+    "url": "/currency-exchange-calculator/static/js/main.00c231ff.chunk.js"
+  },
+  {
+    "revision": "11cc182eeab44c9cd22d",
+    "url": "/currency-exchange-calculator/static/js/runtime-main.dd7dfaf6.js"
+  }
+]);
